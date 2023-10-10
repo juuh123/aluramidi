06011d9c91b4b1c0cce2d7f778f9365257477fac
juuh123 <126296715+juuh123@users.noreply.github.com>
2023-10-10 20:15:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function tocasompom(){
+document.querySelector('# som_tecla_pom').play();
+}
+
+const listaDeBotoes = document.querySelectorAll('.tecla')=tocasompom
+
+listaDeBotoes[0].onclick = tocasompom;
